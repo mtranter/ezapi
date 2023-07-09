50b7b860e02acde5c436d268c83cff786609ca58
Mark Tranter <mark.a.tranter@gmail.com>
2023-07-10 09:38:05 +1000

    Smarten up the CORS middleware

--- packages/middleware/cors/src/cors-middleware.spec.ts
+++ packages/middleware/cors/src/cors-middleware.spec.ts
@@ -1,95 +1,66 @@
 import { CorsMiddleware } from ".";
 
 describe("CorsMiddleware", () => {
-    it("should return 200 OK with CORS headers", async () => {
-      const handler = jest.fn(() => ({
-        statusCode: 200,
-        body: "OK",
-      }));
-      const cors = CorsMiddleware({
-        allowedOrigins: ["https://example.com"],
-        allowedMethods: ["GET", "POST"],
-        allowedHeaders: ["X-Header"],
-        exposedHeaders: ["X-Header"],
-        allowCredentials: true,
-        maxAge: 86400,
-      });
-      const response = await cors(handler)({
-        method: "GET",
-        url: "/",
-        headers: {
-          origin: "https://example.com",
-          "access-control-request-method": "GET",
-          "access-control-request-headers": "X-Header",
-        },
-      });
-      expect(response).toEqual({
-        statusCode: 200,
-        body: "OK",
-        headers: {
-          "access-control-allow-origin": "https://example.com",
-          "access-control-allow-methods": "GET,POST",
-          "access-control-allow-headers": "X-Header",
-          "access-control-expose-headers": "X-Header",
-          "access-control-allow-credentials": "true",
-          "access-control-max-age": "86400",
-        },
-      });
-      expect(handler).toHaveBeenCalledTimes(1);
+  it("should return 200 OK with CORS headers", async () => {
+    const handler = jest.fn(() => ({
+      statusCode: 200,
+      body: "OK",
+    }));
+    const cors = CorsMiddleware({
+      allowedOrigins: ["https://example.com"],
+      allowedMethods: ["GET", "POST"],
+      allowedHeaders: ["X-Header"],
+      exposedHeaders: ["X-Header"],
+      allowCredentials: true,
+      maxAge: 86400,
     });
-  
-    it("should return 403 Forbidden when Origin is not allowed", async () => {
-      const handler = jest.fn(() => ({
-        statusCode: 200,
-        body: "OK",
-      }));
-      const cors = CorsMiddleware({
-        allowedOrigins: ["https://example.com"],
-        allowedMethods: ["GET", "POST"],
-        allowedHeaders: ["X-Header"],
-        exposedHeaders: ["X-Header"],
-        allowCredentials: true,
-        maxAge: 86400,
-      });
-      const response = await cors(handler)({
-        method: "GET",
-        url: "/",
-        headers: {
-          origin: "https://example.org",
-          "access-control-request-method": "GET",
-          "access-control-request-headers": "X-Header",
-        },
-      });
-      expect(response).toMatchObject({
-        statusCode: 403,
-        body: "Forbidden",
-      });
-      expect(handler).toHaveBeenCalledTimes(0);
+    const response = await cors(handler)({
+      method: "OPTIONS",
+      url: "/",
+      headers: {
+        origin: "https://example.com",
+        "access-control-request-method": "GET",
+        "access-control-request-headers": "X-Header",
+      },
     });
-  
-    it("should return 200 OK when Origin is not present", async () => {
-      const handler = jest.fn(() => ({
-        statusCode: 200,
-        body: "OK",
-      }));
-      const cors = CorsMiddleware({
-        allowedOrigins: ["https://example.com"],
-        allowedMethods: ["GET", "POST"],
-        allowedHeaders: ["X-Header"],
-        exposedHeaders: ["X-Header"],
-        allowCredentials: true,
-        maxAge: 86400,
-      });
-      const response = await cors(handler)({
-        method: "GET",
-        headers: {},
-        url: "/",
-      });
-      expect(response).toEqual({
-        statusCode: 200,
-        body: "OK",
-      });
-      expect(handler).toHaveBeenCalledTimes(1);
+    expect(response).toEqual({
+      statusCode: 200,
+      body: "OK",
+      headers: {
+        "access-control-allow-origin": "https://example.com",
+        "access-control-allow-methods": "GET,POST",
+        "access-control-allow-headers": "X-Header",
+        "access-control-expose-headers": "X-Header",
+        "access-control-allow-credentials": "true",
+        "access-control-max-age": "86400",
+        vary: "origin",
+      },
     });
+    expect(handler).toHaveBeenCalledTimes(1);
   });
-  
+
+  it("should return 200 OK when Origin is not present", async () => {
+    const handler = jest.fn(() => ({
+      statusCode: 200,
+      body: "OK",
+    }));
+    const cors = CorsMiddleware({
+      allowedOrigins: ["https://example.com"],
+      allowedMethods: ["GET", "POST"],
+      allowedHeaders: ["X-Header"],
+      exposedHeaders: ["X-Header"],
+      allowCredentials: true,
+      maxAge: 86400,
+    });
+    const response = await cors(handler)({
+      method: "GET",
+      headers: {},
+      url: "/",
+    });
+    expect(response).toEqual({
+      statusCode: 200,
+      body: "OK",
+    });
+    expect(handler).toHaveBeenCalledTimes(1);
+  });
+});
--- packages/middleware/cors/src/cors-middleware.ts
+++ packages/middleware/cors/src/cors-middleware.ts
@@ -1,7 +1,8 @@
-import { Forbidden, HttpMiddleware } from "@ezapi/router-core";
+import { Forbidden, HttpMiddleware, Request } from "@ezapi/router-core";
 
+type AllowedOriginType = string[] | string | RegExp;
 export type CorsConfig = {
-  allowedOrigins: string[];
+  allowedOrigins: string[] | string | RegExp;
   allowedMethods: string[];
   allowedHeaders: string[];
   exposedHeaders: string[];
@@ -9,26 +10,98 @@ export type CorsConfig = {
   maxAge: number;
 };
 
+const configureOriginHeader = (
+  origin: string,
+  originConfig: AllowedOriginType
+): Record<string, string> => {
+  if (typeof originConfig === "string") {
+    if (originConfig === "*") {
+      return {
+        "access-control-allow-origin": originConfig,
+      };
+    } else {
+      return {
+        "access-control-allow-origin": originConfig,
+        vary: "origin",
+      };
+    }
+  }
+  if (Array.isArray(originConfig)) {
+    const isMatch = originConfig.includes(origin);
+    return isMatch
+      ? {
+          "access-control-allow-origin": origin,
+          vary: "origin",
+        }
+      : {};
+  }
+  if (originConfig instanceof RegExp) {
+    const isMatch = originConfig.test(origin);
+    return isMatch
+      ? {
+          "access-control-allow-origin": origin,
+          vary: "origin",
+        }
+      : {};
+  }
+  return {};
+};
+
+const configureAllowedHeaders = (
+  options: CorsConfig,
+  reqHeaders: Record<string, string | string[] | undefined>
+): Record<string, string> => {
+  let allowedHeaders: string[] = options.allowedHeaders;
+  const headers: Record<string, string> = {};
+
+  if (!allowedHeaders.length) {
+    const requestedAllowedHeaders =
+      reqHeaders["access-control-request-headers"];
+    allowedHeaders = requestedAllowedHeaders
+      ? Array.isArray(requestedAllowedHeaders)
+        ? requestedAllowedHeaders
+        : [requestedAllowedHeaders]
+      : [];
+    headers["vary"] = "access-control-request-headers";
+  }
+
+  if (allowedHeaders && allowedHeaders.length) {
+    headers["access-control-allow-headers"] = allowedHeaders.join(",");
+  }
+
+  return headers;
+};
+
 export const CorsMiddleware = <A>(config: CorsConfig) =>
   HttpMiddleware.from<{}, {}, A, A>((handler) => async (req) => {
-    const origin = req.headers.origin as string;
+    const origin = (req.headers.origin || req.headers.Origin) as string;
     if (!origin) {
       return handler(req);
     }
-    if (!config.allowedOrigins.includes(origin)) {
-      return Forbidden("Forbidden" as A);
-    }
+
+    const responseHeaders =
+      req.method === "OPTIONS"
+        ? {
+            ...configureOriginHeader(origin, config.allowedOrigins),
+            ...configureAllowedHeaders(config, req.headers),
+            "access-control-allow-methods": config.allowedMethods.join(","),
+            "access-control-expose-headers": config.exposedHeaders.join(","),
+            "access-control-allow-credentials":
+              config.allowCredentials.toString(),
+            "access-control-max-age": config.maxAge.toString(),
+          }
+        : {
+            ...configureOriginHeader(origin, config.allowedOrigins),
+            "access-control-expose-headers": config.exposedHeaders.join(","),
+            "access-control-allow-credentials":
+              config.allowCredentials.toString(),
+          };
     const resp = await handler(req);
     return {
       ...resp,
       headers: {
         ...resp.headers,
-        "access-control-allow-origin": origin,
-        "access-control-allow-methods": config.allowedMethods.join(","),
-        "access-control-allow-headers": config.allowedHeaders.join(","),
-        "access-control-expose-headers": config.exposedHeaders.join(","),
-        "access-control-allow-credentials": config.allowCredentials.toString(),
-        "access-control-max-age": config.maxAge.toString(),
+        ...responseHeaders,
       },
     };
   });
